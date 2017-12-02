43b7f7bd7235b472cb30a830a93f515c42f0bf69
unknown <Valarie.Regas@gmail.com>
2017-12-01 20:30:34 -0500

    building mission page content

--- controllers/test_controller.js
+++ controllers/test_controller.js
@@ -80,5 +80,36 @@ router.get("/child", function (req, res) {
     res.render('child', {layout: 'child_layout', missions_assigned: missions_assigned, rewards_earned: rewards_earned, child: hero});
 });
 
+// mission_title
+//     mission_point_value
+//     mission_decription
+
+router.get("/missions", function (req, res) {
+
+    var missions_assigned = [
+        {
+            "mission_id": "sample1"
+        },
+        {
+            "mission_id": "sample2"
+        }];
+
+    var mission_name = {
+        "mission_title": "sweep the floor"
+    };
+
+    var mission_pt_val = {
+        "mission_point_value": 9
+    };
+
+    var mission_desc = {
+
+        "mission_description": "Sweep with the broom by making short motions across the floor. Try to be careful not to fling dust everywhere by keeping each sweep short. Sweep all dust" +
+        "and such to one location. sweep everything into the dustpan, then dump the dustpan contents into the trashcan, and put the broom and dustpan away."
+    };
+
+    res.render('missions', {layout: 'child_layout', missions_assigned: missions_assigned, mission_name: mission_name, mission_pt_val: mission_pt_val, mission_desc: mission_desc});
+})
+
 // Export routes for server.js to use.
 module.exports = router;
