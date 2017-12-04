9785336f00fc65ad956edb248cf93611947afc68
Kathy Satterlee <k.j.satterlee@gmail.com>
2017-12-04 13:41:00 -0500

    adding associations

---
+++ controllers/parent_api_controller.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var db = require("../models");
+var router = express.Router();
+
+router.get("/api/parents", function (req, res) {
+  db.Parents.findOne({
+    where: {
+      uid: req.params.uid
+    },
+    include: [db.children]
+  }).then(function (dbParent) {
+    return dbParent
+  })
+})
--- models/MissionsActive.js
+++ models/MissionsActive.js
@@ -1,11 +1,29 @@
-module.exports = function (sequelize, Datatypes) {
-  const MissionsActive = sequelize.define("MissionsActive", {
-    status: {
-      type: Datatypes.STRING,
+module.exports = function (sequelize, DataTypes) {
+  const MissionsActive = sequelize.define("active_missions", {
+    mission_status: {
+      type: DataTypes.STRING,
       allowNull: false,
       default: "i"
+    },
+    mission_status_date: {
+     type: DataTypes.DATEONLY,
+     allowNull: false,
+     defaultValue: DataTypes.NOW
     }
 
-  })
+  });
+  /*MissionsActive.associate = function (models) {
+    MissionsActive.belongsTo(models.Missions, {
+      foreignKey: {
+        allowNull: false
+      }
+    }),
+    MissionsActive.belongsTo(models.Children, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+
+  };*/
   return MissionsActive
 };
