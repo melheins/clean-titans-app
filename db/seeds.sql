f6f9a98fb936e4dfa3072ef8c8c93b60c70553e1
melheins <heins.mel@gmail.com>
2017-12-04 22:35:53 -0500

    started mission/rewards seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,6 +9,10 @@ USE clean_titans_db;
 
 
 --##Missions Data##
-
+INSERT INTO missions (mission_title, mission_point_value, mission_decription, createdAt,updatedAt)
+VALUES ('Brush Teeth AM', 3,'Brush Teeth AM', current_timestamp(),current_timestamp());
 
 --##Rewards Data##
+INSERT INTO rewards (reward_name, reward_description,reward_points_required,reward_category, createdAt,updatedAt)
+VALUES ('Ice Cream', 'Ice Cream treat',10,'r', current_timestamp(),current_timestamp());
+
